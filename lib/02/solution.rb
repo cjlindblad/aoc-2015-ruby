62e684864de57491d8be65f7bb7b8788a3e89841
cj lindblad <carljohan.lindblad@gmail.com>
2020-06-28 19:59:27 +0200

    Day 2 part 2 solved

--- lib/02/solution.rb
+++ lib/02/solution.rb
@@ -8,6 +8,15 @@ class Calculator
     sum
   end
 
+  def self.calculate_ribbon(instructions)
+    sum = 0
+    instructions.each do |instruction|
+      box = Box.new(*self.parse(instruction))
+      sum += box.ribbon_demand
+    end
+    sum
+  end
+
   def self.parse(instruction)
     instruction.split('x').map { |char| char.to_i }
   end
@@ -27,5 +36,12 @@ class Box
     sorted_sides = [side_a, side_b, side_c].sort
     smallest_side = sorted_sides.first
     (sorted_sides.sum * 2) + smallest_side
+  end
+
+  def ribbon_demand
+    sorted_sides = [@width, @depth, @height].sort
+    smallest_perimiter = sorted_sides[0..1].sum * 2
+    bow = @width * @depth * @height
+    smallest_perimiter + bow
   end
 end
--- spec/02_spec.rb
+++ spec/02_spec.rb
@@ -19,3 +19,23 @@ RSpec.describe 'day 2 part 1 solution' do
     end
   end
 end
+
+RSpec.describe 'day 2 part 2 solution' do
+  it 'needs 34 feet of ribbon for 2x3x4 box' do
+    result = Calculator.calculate_ribbon(['2x3x4'])
+    expect(result).to eq 34
+  end
+
+  it 'needs 14 feet of ribbon for 1x1x10 box' do
+    result = Calculator.calculate_ribbon(['1x1x10'])
+    expect(result).to eq 14
+  end
+
+  it 'solves part 2' do
+    File.open('lib/02/input.txt') do |file|
+      input = file.read
+      result = Calculator.calculate_ribbon(input.split("\n"))
+      expect(result).to eq 3783758
+    end
+  end
+end
