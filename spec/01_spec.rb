88df35fae41bc7e23a44e1df0c84787eb6a89661
cj lindblad <carljohan.lindblad@gmail.com>
2020-06-28 12:34:18 +0200

    Solved day 2 part 2

--- spec/01_spec.rb
+++ spec/01_spec.rb
@@ -31,3 +31,19 @@ RSpec.describe 'day 1 solution part 1' do
     expect(Parser.parse(input)).to eq 232
   end
 end
+
+RSpec.describe 'day 1 solution part 2' do
+  it 'finds position 1 from )' do
+    expect(Parser.find_basement_entry_position(')')).to eq 1
+  end
+
+  it 'finds position 5 from ()())' do
+    expect(Parser.find_basement_entry_position('()())')).to eq 5
+  end
+
+  it 'solves part 2' do
+    file = File.open('lib/01/input.txt')
+    input = file.read
+    expect(Parser.find_basement_entry_position(input)).to eq 1783
+  end
+end
