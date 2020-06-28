2730730bc7b141ddeb03761a9dbc73bc50cbba3c
cj lindblad <carljohan.lindblad@gmail.com>
2020-06-28 13:09:31 +0200

    Day 2 part 1 solved

--- lib/01/elevator_direction_parser.rb
+++ lib/01/elevator_direction_parser.rb
@@ -2,6 +2,7 @@ class Parser
   def self.parse(direction)
     direction.count('(') - direction.count(')')
   end
+
   def self.find_basement_entry_position(direction)
     floor = 0
     direction.split('').each_with_index do |char, index|
---
+++ spec/02_spec.rb
@@ -0,0 +1,21 @@
+require '02/gift_wrap_calculator'
+
+RSpec.describe 'day 2 part 1 solution' do
+  it 'needs 58 square feet from 2x3x4 box' do
+    result = Calculator.calculate_paper(['2x3x4'])
+    expect(result).to eq 58
+  end
+
+  it 'needs 43 square feet from 1x1x10 box' do
+    result = Calculator.calculate_paper(['1x1x10'])
+    expect(result).to eq 43
+  end
+
+  it 'solves part 1' do
+    File.open('lib/02/input.txt') do |file|
+      input = file.read
+      result = Calculator.calculate_paper(input.split("\n"))
+      expect(result).to eq 1588178
+    end
+  end
+end
