34eb7d246cdd78856aa50c3f66f192ad8b5fc07d
cj lindblad <carljohan.lindblad@gmail.com>
2020-06-29 20:45:35 +0200

    Use RSpec tags to filter out expensive tests

--- spec/04_spec.rb
+++ spec/04_spec.rb
@@ -1,21 +1,21 @@
 require '04/solution'
 
 RSpec.describe "Day 4 part 1 solution" do
-  it "finds 609043 for abcdef" do
+  it "finds 609043 for abcdef", :speed => :slow do
     match = /\A0{5}/
     miner = Miner.new 'abcdef'
     result = miner.find match
     expect(result).to eq 609043
   end
   
-  it "finds 1048970 for pqrstuv" do
+  it "finds 1048970 for pqrstuv", :speed => :slow do
     match = /\A0{5}/
     miner = Miner.new 'pqrstuv'
     result = miner.find match
     expect(result).to eq 1048970
   end
 
-  it "solves part 1" do
+  it "solves part 1", :speed => :slow do
     match = /\A0{5}/
     miner = Miner.new 'ckczppom'
     result = miner.find match
@@ -23,7 +23,7 @@ RSpec.describe "Day 4 part 1 solution" do
   end
 end
 
-RSpec.describe "Day 4 part 2 solution" do
+RSpec.describe "Day 4 part 2 solution", :speed => :slow do
   it "solves part 2" do
     match = /\A0{6}/
     miner = Miner.new 'ckczppom'
