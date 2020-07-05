e92257813f7aa8b82cd4f2301217decb21833a87
cj lindblad <carljohan.lindblad@gmail.com>
2020-07-05 09:44:48 +0200

    Day 12 part 2 solved

--- lib/12/solution.rb
+++ lib/12/solution.rb
@@ -5,16 +5,20 @@ class DocumentAnalyzer
     @document = JSON.parse(string)
   end
 
-  def sum
+  def sum avoid=nil
     @sum = 0
-    traverse @document
+    traverse @document, avoid
     @sum
   end
 
-  def traverse node
+  def traverse node, avoid
     node.each do |e|
+      if e.is_a? Hash and avoid != nil and e.has_value? avoid
+        next
+      end
+
       if e.is_a? Array or e.is_a? Hash
-        traverse e
+        traverse e, avoid
       end
 
       @sum += e if e.is_a? Integer
--- spec/12_spec.rb
+++ spec/12_spec.rb
@@ -7,4 +7,11 @@ describe DocumentAnalyzer do
     result = document.sum
     expect(result).to eq 119433
   end
+
+  it "solves part 2" do
+    input = File.read('lib/12/input.txt')
+    document = DocumentAnalyzer.new input
+    result = document.sum "red"
+    expect(result).to eq 68466
+  end
 end
