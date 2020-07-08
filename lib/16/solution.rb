390c30316012a60ae586a4b8146a60c0dd8a17ee
cj lindblad <carljohan.lindblad@gmail.com>
2020-07-08 21:20:04 +0200

    Day 16 part 2 solved

--- lib/16/solution.rb
+++ lib/16/solution.rb
@@ -1,26 +1,3 @@
-class MFCSAM
-  def initialize
-    ticker_sue_input = "children: 3, cats: 7, samoyeds: 2, pomeranians: 3, akitas: 0, vizslas: 0, goldfish: 5, trees: 3, cars: 2, perfumes: 1"
-    @ticker_sue = Sue.from_string ticker_sue_input
-    @sues = []
-  end
-
-  def add_sue input
-    sue = Sue.from_string input
-    @sues << sue
-  end
-
-  def find_matching_sue
-    target_sue = 0
-    @sues.each do |sue|
-      if sue.compounds - @ticker_sue.compounds == []
-        target_sue = sue.number
-      end
-    end
-    target_sue
-  end
-end
-
 class Sue
   attr_reader :number, :compounds
 
@@ -37,7 +14,7 @@ class Sue
     rest.split(',').each do |compound_string|
       name = compound_string.match /\w+/
       amount = compound_string.match /\d+/
-      compound = Compound.new name.to_s, amount.to_s.to_i
+      compound = Compound.new name.to_s.to_sym, amount.to_s.to_i
       compounds.push compound
     end
     Sue.new sue_number, compounds
@@ -60,12 +37,48 @@ class Compound
     other.name == @name && other.amount == @amount
   end
 
-  def hash
-    @name.hash * @amount.hash
-  end
-
   def to_s
     "#{@name}: #{@amount}"
   end
 end
+
+class MFCSAM
+  def initialize part=:part1
+    ticker_sue_input = "children: 3, cats: 7, samoyeds: 2, pomeranians: 3, akitas: 0, vizslas: 0, goldfish: 5, trees: 3, cars: 2, perfumes: 1"
+    @ticker_sue = Sue.from_string ticker_sue_input
+    @sues = []
+
+    if part == :part2
+      Compound.class_eval do
+        def eql?(other)
+          return false if @name != other.name
+          case @name
+          when :cats, :trees
+            @amount > other.amount
+          when :goldfish, :pomeranians
+            @amount < other.amount
+          else
+            @amount == other.amount
+          end
+        end
+      end
+    end
+  end
+
+  def add_sue input
+    sue = Sue.from_string input
+    @sues << sue
+  end
+
+  def find_matching_sue
+    target_sue = 0
+    @sues.each do |sue|
+      if sue.compounds - @ticker_sue.compounds == []
+        target_sue = sue.number
+      end
+    end
+    target_sue
+  end
+end
+
 
--- spec/16_spec.rb
+++ spec/16_spec.rb
@@ -11,4 +11,15 @@ describe "day 16" do
     matching_sue = mfcsam.find_matching_sue
     expect(matching_sue).to eq 373
   end
+
+  it "solves part 2" do
+    mfcsam = MFCSAM.new :part2
+    File.open('lib/16/input.txt') do |file|
+      file.each_line do |line|
+        mfcsam.add_sue line
+      end
+    end
+    matching_sue = mfcsam.find_matching_sue
+    expect(matching_sue).to eq 260
+  end
 end
