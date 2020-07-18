7f99b395dbaec6b9c1351630b77c6ddb19f57032
cj lindblad <carljohan.lindblad@gmail.com>
2020-07-18 12:13:27 +0200

    Day 20 part 2 solved

--- lib/20/solution.rb
+++ lib/20/solution.rb
@@ -16,4 +16,25 @@ class HouseFinder
       return house_number if presents >= goal
     end
   end
+  
+  def find_2(goal)
+    max_house = goal / 10
+    houses = [0]
+    1.upto(max_house) do |house_number|
+      houses[house_number] = 0
+    end
+    1.upto(max_house) do |elf_number|
+      house = elf_number
+      iterations = 0
+      until house > max_house || iterations == 50 do
+        houses[house] += elf_number * 11
+        house += elf_number
+        iterations += 1
+      end
+    end
+    houses.each.with_index do |presents, house_number|
+      return house_number if presents >= goal
+    end
+  end
+
 end
