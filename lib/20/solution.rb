a238bc857345b7b2a07ecb0f14dde74a84795b3f
cj lindblad <carljohan.lindblad@gmail.com>
2020-07-18 12:36:01 +0200

    Refactored day 20

--- lib/20/solution.rb
+++ lib/20/solution.rb
@@ -1,40 +1,37 @@
 class HouseFinder
-  def find(goal)
-    max_house = goal / 10
-    houses = [0]
-    1.upto(max_house) do |house_number|
-      houses[house_number] = 0
-    end
-    1.upto(max_house) do |elf_number|
-      house = elf_number
-      until house > max_house do
-        houses[house] += elf_number * 10
-        house += elf_number
-      end
-    end
-    houses.each.with_index do |presents, house_number|
-      return house_number if presents >= goal
+  def initialize goal, multiplier, elf_limit: nil
+    @houses = [0]
+    @max_house = goal / 10
+    @multiplier = multiplier
+    @elf_limit = elf_limit
+    @goal = goal
+    
+    initialize_houses
+  end
+
+  def initialize_houses
+    1.upto(@max_house) do |house_number|
+      @houses[house_number] = 0
     end
   end
-  
-  def find_2(goal)
-    max_house = goal / 10
-    houses = [0]
-    1.upto(max_house) do |house_number|
-      houses[house_number] = 0
+
+  def find
+    deliver_presents
+
+    @houses.each.with_index do |presents, house_number|
+      return house_number if presents >= @goal
     end
-    1.upto(max_house) do |elf_number|
+  end
+
+  def deliver_presents
+    1.upto(@max_house) do |elf_number|
       house = elf_number
       iterations = 0
-      until house > max_house || iterations == 50 do
-        houses[house] += elf_number * 11
+      until house > @max_house || (@elf_limit != nil && iterations == @elf_limit) do
+        @houses[house] += elf_number * @multiplier
         house += elf_number
         iterations += 1
       end
     end
-    houses.each.with_index do |presents, house_number|
-      return house_number if presents >= goal
-    end
   end
-
 end
--- spec/20_spec.rb
+++ spec/20_spec.rb
@@ -2,30 +2,30 @@ require '20/solution'
 
 describe HouseFinder do
   it "finds that house #8 is the first to receive 150 presents" do
-    finder = HouseFinder.new
     present_goal = 150
-    house_number = finder.find(present_goal)
+    finder = HouseFinder.new(present_goal, 10)
+    house_number = finder.find
     expect(house_number).to eq 8
   end
 
   it "find that house #12 is the first to receive 280 presents" do
-    finder = HouseFinder.new
     present_goal = 280
-    house_number = finder.find(present_goal)
+    finder = HouseFinder.new(present_goal, 10)
+    house_number = finder.find
     expect(house_number).to eq 12
   end
 
   it "solves part 1", :speed => :slow do
-    finder = HouseFinder.new
     present_goal = 33100000
-    house_number = finder.find(present_goal)
+    finder = HouseFinder.new(present_goal, 10)
+    house_number = finder.find
     expect(house_number).to eq 776160
   end
 
-  it "solves part 2" do
-    finder = HouseFinder.new
+  it "solves part 2", :speed => :slow do
     present_goal = 33100000
-    house_number = finder.find_2(present_goal)
+    finder = HouseFinder.new(present_goal, 11, elf_limit: 50)
+    house_number = finder.find
     expect(house_number).to eq 786240
   end
 end
